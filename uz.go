9deabb4048a54565bb92976eaa83360c560a4706
Yoshihiro Fukushima <y.of.u.142857@gmail.com>
2015-02-25 18:46:52 +0900

    go fmt

--- uz.go
+++ uz.go
@@ -1,81 +1,81 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "archive/zip"
-    "io"
-    "os"
-    "path/filepath"
-    "code.google.com/p/go.text/encoding"
-    "code.google.com/p/go.text/encoding/japanese"
-    "code.google.com/p/go.text/transform"
+	"archive/zip"
+	"code.google.com/p/go.text/encoding"
+	"code.google.com/p/go.text/encoding/japanese"
+	"code.google.com/p/go.text/transform"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func Input(question string) string {
-    fmt.Print(question)
-    var answer string
-    fmt.Scanln(&answer)
-    return answer
+	fmt.Print(question)
+	var answer string
+	fmt.Scanln(&answer)
+	return answer
 }
 
 func UnZip(fn, dist string) error {
-    r, err := zip.OpenReader(fn)
-    if err != nil {
-        return err
-    }
-    defer r.Close()
-    for _, f := range r.File {
-        rc, err := f.Open()
-        if err != nil {
-            return err
-        }
-        defer rc.Close()
-        var path string
-        _, _, err = transform.String(encoding.UTF8Validator, f.Name)
-        if err != nil {
-            for _, enc := range []encoding.Encoding{japanese.ShiftJIS, japanese.EUCJP, japanese.ISO2022JP} {
-                tmp, _, err := transform.String(enc.NewDecoder(), f.Name)
-                if err != nil {
-                    continue
-                }
-                path = filepath.Join(dist, tmp)
-                break
-            }
-        } else {
-            path = filepath.Join(dist, f.Name)
-        }
-        fmt.Printf("OUTPUT: %s\n", path)
-        if f.FileInfo().IsDir() {
-            os.MkdirAll(path, f.Mode())
-        } else {
-            os.MkdirAll(filepath.Dir(path), f.Mode())
-            tmpf, err := os.Create(path)
-            if err != nil {
-                return err
-            }
-            defer tmpf.Close()
-            _, err = io.Copy(tmpf, rc)
-            if err != nil {
-                return err
-            }
-        }
-    }
-    return nil
+	r, err := zip.OpenReader(fn)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+		var path string
+		_, _, err = transform.String(encoding.UTF8Validator, f.Name)
+		if err != nil {
+			for _, enc := range []encoding.Encoding{japanese.ShiftJIS, japanese.EUCJP, japanese.ISO2022JP} {
+				tmp, _, err := transform.String(enc.NewDecoder(), f.Name)
+				if err != nil {
+					continue
+				}
+				path = filepath.Join(dist, tmp)
+				break
+			}
+		} else {
+			path = filepath.Join(dist, f.Name)
+		}
+		fmt.Printf("OUTPUT: %s\n", path)
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			tmpf, err := os.Create(path)
+			if err != nil {
+				return err
+			}
+			defer tmpf.Close()
+			_, err = io.Copy(tmpf, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func main() {
-    var inp string
-    var err error
+	var inp string
+	var err error
 
-    if len(os.Args) < 2 {
-        inp = Input("File Name: ")
-    } else {
-        inp = os.Args[1]
-    }
-    dst := filepath.Join(filepath.Dir(inp), "out")
-    err = UnZip(inp, dst)
-    if err != nil {
-        log.Fatal(err)
-    }
+	if len(os.Args) < 2 {
+		inp = Input("File Name: ")
+	} else {
+		inp = os.Args[1]
+	}
+	dst := filepath.Join(filepath.Dir(inp), "out")
+	err = UnZip(inp, dst)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
